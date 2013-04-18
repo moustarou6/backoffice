137f3aedaee87bf72a58a3b06bbcfbdc9dfa1f82
mika <mickael.hamour@e-fijy.com>
2013-04-18 11:16:23 +0200

    bdd

--- localhost.sql
+++ localhost.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Lun 15 Avril 2013 à 13:17
+-- Généré le: Jeu 18 Avril 2013 à 11:14
 -- Version du serveur: 5.5.25a-log
 -- Version de PHP: 5.4.4
 
@@ -25,18 +25,49 @@ USE `engine`;
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `portal`
+--
+
+CREATE TABLE IF NOT EXISTS `portal` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `x` int(11) NOT NULL,
+  `y` int(11) NOT NULL,
+  `z` int(11) NOT NULL,
+  `code` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `portal`
+--
+
+INSERT INTO `portal` (`id`, `x`, `y`, `z`, `code`) VALUES
+(1, 42, 798, 4, 'AZER'),
+(2, 897, 435, 798, 'POI');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `spaceship`
 --
 
 CREATE TABLE IF NOT EXISTS `spaceship` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `path` varchar(50) NOT NULL,
   `price` int(11) NOT NULL,
   `module_limit` int(11) NOT NULL,
   `construct_time` int(11) NOT NULL,
+  `power` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `spaceship`
+--
+
+INSERT INTO `spaceship` (`id`, `name`, `path`, `price`, `module_limit`, `construct_time`, `power`) VALUES
+(1, 'default', 'default.fbx', 1500, 2, 24, 20);
 
 -- --------------------------------------------------------
 
@@ -76,13 +107,24 @@ CREATE TABLE IF NOT EXISTS `type` (
 --
 
 CREATE TABLE IF NOT EXISTS `user_spaceship` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `spaceship_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
+  `life` int(11) NOT NULL,
+  `position_X` int(11) NOT NULL,
+  `position_Y` int(11) NOT NULL,
+  `position_Z` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `spaceship_id` (`spaceship_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `user_spaceship`
+--
+
+INSERT INTO `user_spaceship` (`id`, `spaceship_id`, `user_id`, `life`, `position_X`, `position_Y`, `position_Z`) VALUES
+(1, 1, 5, 7, 10, 10, 10);
 
 -- --------------------------------------------------------
 
@@ -91,9 +133,9 @@ CREATE TABLE IF NOT EXISTS `user_spaceship` (
 --
 
 CREATE TABLE IF NOT EXISTS `user_spaceship_module` (
-  `user_id` int(11) NOT NULL,
+  `user_spaceship` int(11) NOT NULL,
   `spaceship_module_id` int(11) NOT NULL,
-  KEY `user_id` (`user_id`),
+  KEY `user_id` (`user_spaceship`),
   KEY `spaceship_module_id` (`spaceship_module_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -134,12 +176,12 @@ ALTER TABLE `spaceship_module`
 -- Contraintes pour la table `user_spaceship`
 --
 ALTER TABLE `user_spaceship`
-  ADD CONSTRAINT `user_spaceship_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `user_spaceship_ibfk_1` FOREIGN KEY (`spaceship_id`) REFERENCES `spaceship` (`id`);
+  ADD CONSTRAINT `user_spaceship_ibfk_1` FOREIGN KEY (`spaceship_id`) REFERENCES `spaceship` (`id`),
+  ADD CONSTRAINT `user_spaceship_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Contraintes pour la table `user_spaceship_module`
 --
 ALTER TABLE `user_spaceship_module`
   ADD CONSTRAINT `user_spaceship_module_ibfk_2` FOREIGN KEY (`spaceship_module_id`) REFERENCES `spaceship_module` (`id`),
-  ADD CONSTRAINT `user_spaceship_module_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `user_spaceship_module_ibfk_3` FOREIGN KEY (`user_spaceship`) REFERENCES `user_spaceship` (`id`);
